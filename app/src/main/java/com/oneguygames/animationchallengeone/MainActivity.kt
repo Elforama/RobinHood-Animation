57bdbcfb4ae739598e674bc6b1722427a65bcdbd
Lawrence Muller <lawrencejmuller@gmail.com>
2017-12-16 21:56:17 -0500

    inirial commit

---
+++ app/src/main/java/com/oneguygames/animationchallengeone/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.oneguygames.animationchallengeone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.constraint.ConstraintSet
+import android.support.transition.AutoTransition
+import android.support.transition.Transition
+import android.support.transition.TransitionManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity: AppCompatActivity() {
+
+    var changed = false
+    val setOne = ConstraintSet()
+    val setTwo = ConstraintSet()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setOne.clone(root)
+
+        setTwo.clone(this, R.layout.activity_main_two)
+
+
+        floatingActionButton.setOnClickListener {
+           animate()
+        }
+
+    }
+
+    fun animate() {
+
+        val t = AutoTransition()
+
+
+        TransitionManager.beginDelayedTransition(root, t)
+        if (!changed) setTwo.applyTo(root) else setOne.applyTo(root)
+        changed = !changed
+    }
+
+    override fun onBackPressed() {
+        if (changed) {
+            animate()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
